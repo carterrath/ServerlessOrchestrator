5d14cce90f60d82d1e63cca599630d94cf2c3d4a
ruthijimenez <69929099+ruthijimenez@users.noreply.github.com>
2023-10-27 17:59:10 -0700

    Set up SQLite using testRepo link!

--- main.go
+++ main.go
@@ -1,1 +1,11 @@
 package main
+
+import (
+	"github.com/GoKubes/ServerlessOrchestrator/dataAccess"
+	"github.com/GoKubes/ServerlessOrchestrator/db"
+)
+
+func main() {
+	db.CreateDatabase()
+	dataAccess.ConnectToDB()
+}
