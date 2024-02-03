0777a75225947f177ad2ebe2c848f251c50a2295
carterrath <carterrathh@gmail.com>
2024-02-03 14:52:13 -0800

    Added pages

--- presentation/src/App.tsx
+++ presentation/src/App.tsx
@@ -1,11 +1,39 @@
 import './assets/styles/App.css'
 import { Router } from './router'
+import { useNavigate } from 'react-router-dom';
+
 
 function App() {
-
+  
+  const navigate = useNavigate();
+  
+  const handleDeveloperLogin = () => {
+    navigate('/developeroptions');
+  };
+  
   return (
-    <Router/>
-  );
-}
-
-export default App
+    <div className="App">
+      <Router/>
+      // This is your home page layout with login options
+      <header className="App-header">
+      <h1>Serverless Orchestrator</h1>
+      <div className="login-container">
+      <div className="login developer-login">
+      <h2>Developer</h2>
+      <input type="text" placeholder="Username" />
+      <input type="password" placeholder="Password" />
+      <button onClick={handleDeveloperLogin}>Login</button>
+      </div>
+      <div className="login user-login">
+      <h2>Consumer</h2>
+      <input type="text" placeholder="Username" />
+      <input type="password" placeholder="Password" />
+      <button>Login</button>
+      </div>
+      </div>
+      </header>
+    </div>
+    );
+  }
+  
+  export default App;
---
+++ presentation/src/pages/Developer/index.tsx
@@ -0,0 +1,18 @@
+
+import './Developer.css'; 
+
+function Developer() {
+  return (
+    <div className="developer-page">
+    <header className="Developer-header">
+    <h1>Connect your app!</h1>
+    <input type="text" placeholder="Enter GitHub Repository Link" />
+    <button>Connect</button>
+    {/* ... rest of the content based on the screenshot */}
+    </header>
+    </div>
+    );
+  }
+  
+  export default Developer;
+  
