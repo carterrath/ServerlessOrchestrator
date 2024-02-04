fe97a35850286f764cd8c955d1649c919b0cade9
ruthijimenez <69929099+ruthijimenez@users.noreply.github.com>
2024-02-03 19:39:39 -0800

    updated from react router branch

--- presentation/src/pages/Developer/index.tsx
+++ presentation/src/pages/Developer/index.tsx
@@ -4,11 +4,11 @@ import './../../assets/styles/Developer.css';
 function Developer() {
   return (
     <div className="developer-page">
-    <header className="Developer-header">
-    <h1>Connect your app!</h1>
+    <header className="developer-page-header">
+    <h1>Serverless Orchestrator</h1>
+    <h2>Connect your app!</h2>
     <input type="text" placeholder="Enter GitHub Repository Link" />
     <button>Connect</button>
-    {/* ... rest of the content based on the screenshot */}
     </header>
     </div>
     );
--- presentation/src/pages/Home/index.tsx
+++ presentation/src/pages/Home/index.tsx
@@ -1,10 +1,13 @@
 import { Link } from "react-router-dom";
+import './../../assets/styles/App.css';
 
 export function Home() {
   return (
+    <div className="App">
     <div>
       <h1>Home</h1>
       <Link to="/Login">Login</Link>
+    </div>
     </div>
   );
 }
