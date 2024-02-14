b807013bcb230d5c7dcf87f2fd8e4edb69e7eec1
carterrath <carterrathh@gmail.com>
2024-02-13 22:56:54 -0800

    added logo

--- presentation/src/components/NavBar/UpperNav/index.tsx
+++ presentation/src/components/NavBar/UpperNav/index.tsx
@@ -2,9 +2,18 @@
 export function UpperNav(){
     return(
         <ul className="c-nav-links c-nav-left">
-            
+            <li>
+                <a className="navbar-brand" href="/Home">
+                    <img src="/src/assets/images/logo.png" alt="Logo" 
+                    style={{maxHeight:  "35px", marginLeft: "25px", marginTop: "-8px"}}
+                    /> <b> Serverless Orchestrator</b>
+                </a>
+            </li>
             <li>
                 <a href="/Home">Home</a>
+            </li>
+            <li>
+                <a href="/Microservices">Microservices</a>
             </li>
             <li>
                 <a href="/Login">Login</a>
