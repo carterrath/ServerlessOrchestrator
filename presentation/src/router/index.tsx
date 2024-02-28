cf3119f0348a8f572b2a61926ebf2e2dc6b1f271
Jaclyn Walsh <jaclynewalsh@gmail.com>
2024-02-28 13:15:51 -0800

    Attempting to fix merge with main

--- presentation/src/router/index.tsx
+++ presentation/src/router/index.tsx
@@ -1,23 +1,29 @@
 //import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { Home } from '../pages/Home';
-import { Login } from '../pages/Login';
 import { UploadMicroservice } from '../pages/UploadMicroservice';
 import DeveloperOptions from '../pages/DeveloperOptions';
 import { Microservices } from '../pages/Microservices';
 import { NavBar } from '../components/NavBar';
-
+import MainChoice from '../pages/GetStarted/index';
+import DeveloperSignup from '../pages/GetStarted/DeveloperSignup';
+import ConsumerSignup from '../pages/GetStarted/ConsumerSignup';
+import DeveloperLogin from '../pages/GetStarted/DeveloperLogin';
+import ConsumerLogin from '../pages/GetStarted/ConsumerLogin';
 export function Router() {
   return (
     <BrowserRouter>
     <NavBar isDeveloper={true} />
       <Routes>
         <Route path="/*"  element={<Home />} />
-        <Route path="/Login"  element={<Login />} />
         <Route path="/UploadMicroservice" element={<UploadMicroservice />} />
         <Route path="/DeveloperOptions" element={<DeveloperOptions />} />
         <Route path="/Microservices" element={<Microservices />} />
-    
+        <Route path="/GetStarted" element={<MainChoice />} />
+        <Route path="/developer-signup" element={<DeveloperSignup />} />
+        <Route path="/consumer-signup" element={<ConsumerSignup />} />
+        <Route path="/developer-login" element={<DeveloperLogin />} />
+        <Route path="/consumer-login" element={<ConsumerLogin />} />
       </Routes>
     </BrowserRouter>
   );
