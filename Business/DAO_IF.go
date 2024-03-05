ed77f30c374dccbb4d852e744a7872645de86088
ruthijimenez <69929099+ruthijimenez@users.noreply.github.com>
2024-03-04 18:57:28 -0800

    removed deaed code

--- Business/DAO_IF.go
+++ Business/DAO_IF.go
@@ -2,27 +2,12 @@ package business
 
 // DAO_IF is an interface that defines methods for interacting with microservices data.
 type DAO_IF interface {
-	// GetAll retrieves all microservices from the database.
-	// GetAll() ([]Microservice, error)
-
-	// Insert adds a new microservice record to the database.
-	// Insert(micro Microservice) error
-
-	// Delete removes a microservice record from the database by ID.
-	// Delete(id uint) error
-
-	// GetByID retrieves a microservice by its ID.
-	// GetByID(id uint) (Microservice, error)
-
-	// Update modifies an existing microservice record.
-	// Update(micro Microservice) error
-
 	// Insert adds a new entity record to the database.
-    Insert(entity interface{}) error
+	Insert(entity interface{}) error
 
-    // Delete removes an entity record from the database by ID.
-    Delete(id uint) error
+	// Delete removes an entity record from the database by ID.
+	Delete(id uint) error
 
-    // GetByID retrieves an entity by its ID.
-    GetByID(id uint) (interface{}, error)
+	// GetByID retrieves an entity by its ID.
+	GetByID(id uint) (interface{}, error)
 }
