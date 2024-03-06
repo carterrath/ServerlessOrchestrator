be8fbaf4f2a27b0077e1feaf293a5ec856a3bdf2
carterrath <carterrathh@gmail.com>
2024-03-06 14:40:48 -0800

    Update index.tsx

--- presentation/src/components/NavBar/index.tsx
+++ presentation/src/components/NavBar/index.tsx
@@ -6,19 +6,19 @@ interface IProps{
 export function NavBar(props: IProps){
     return(
         <div className="top-0 z-[999999999999] sticky w-full h-20 px-4 items-center flex drop-shadow-xl bg-gradient-to-r from-darkPink to-amaranthPink justify-between">
-            <div className="flex gap-4 items-center">
-                <Link className="flex items-center hover:scale-105 transition duration-150 ease-in-out" to="/Home">
+            <div className="flex gap-6 items-center">
+                <Link className="flex items-center hover:scale-105 transition duration-150 ease-in-out text-xl" to="/Home">
                     <img className="w-8 h-8" src="/src/assets/images/logo.png" alt="Logo"/> 
                      &nbsp;<b>Serverless Orcastrator</b>
                 </Link>
-                <Link className="hover:scale-105 transition duration-150 ease-in-out" to="/Home">
+                <Link className="hover:scale-105 transition duration-150 ease-in-out text-white" to="/Home">
                     Home
                 </Link>
-                <Link className="hover:scale-105 transition duration-150 ease-in-out" to="/Microservices">
+                <Link className="hover:scale-105 transition duration-150 ease-in-out text-white" to="/Microservices">
                     Microservices
                 </Link>
                 {props.isDeveloper && (
-                    <Link className="hover:scale-105 transition duration-150 ease-in-out" to="/UploadMicroservice">
+                    <Link className="hover:scale-105 transition duration-150 ease-in-out text-white" to="/UploadMicroservice">
                         Upload
                     </Link>
                 )}     
