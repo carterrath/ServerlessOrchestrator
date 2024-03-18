263cc96faf7bf15a9790d621cb28ab322daaabd4
Jaclyn Walsh <jaclynewalsh@gmail.com>
2024-03-18 16:03:55 -0700

    Update index.tsx

--- presentation/src/components/TechStackSection/index.tsx
+++ presentation/src/components/TechStackSection/index.tsx
@@ -9,12 +9,12 @@ import kubernetesWhiteIcon from '../../assets/svg/kubernetes-logo-white.svg';
 import { Fade } from 'react-awesome-reveal';
 
 export function TechStackSection() {
-    return (
-      <Fade>
+  return (
+    <Fade>
       <div className="bg-gradient-to-r from-darkPink to-amaranthPink py-24 sm:py-32">
         <div className="mx-auto max-w-7xl px-6 lg:px-8">
-          <h2 className="text-center text-lg font-semibold leading-8 text-white">
-            Built with the best tools in the industry 🛠
+          <h2 className="text-center text-lg font-semibold leading-8 text-white p-4">
+            Built with the best tools in the industry
           </h2>
           <div className="mx-auto mt-10 grid max-w-lg grid-cols-7 items-center gap-x-8 gap-y-10 sm:max-w-xl sm:gap-x-10 lg:mx-0 lg:max-w-none">
             <img
@@ -69,7 +69,6 @@ export function TechStackSection() {
           </div>
         </div>
       </div>
-      </Fade>
-    );
-  }
-  
+    </Fade>
+  );
+}
