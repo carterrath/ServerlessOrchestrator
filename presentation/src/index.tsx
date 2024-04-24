fd762a88a9b0e78e95db4b2e827f6e12ba486b16
carterrath <carterrathh@gmail.com>
2024-04-23 19:51:32 -0700

    uninstall react query

--- presentation/src/index.tsx
+++ presentation/src/index.tsx
@@ -1,17 +1,12 @@
 import { Router } from './router';
 //import './assets/styles/main.less'
 import './assets/styles/styles.css';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-
-const queryClient = new QueryClient();
 
 function App() {
   return (
-    <QueryClientProvider client={queryClient}>
-      <div className="min-h-svh">
-        <Router />
-      </div>
-    </QueryClientProvider>
+    <div className="min-h-svh">
+      <Router />
+    </div>
   );
 }
 
